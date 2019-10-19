876cf5dcf9a805bf5d3d0142661b471d41d173d5
Pig <pig.priv@gmail.com>
2019-10-19 22:52:07 +0200

    drm: Add panel info sysfs node
     * ADSP reads it.
     W /vendor/bin/adsprpcd:
     vendor/qcom/proprietary/commonsys-intf/adsprpc/src/apps_std_imp.c:749:
     Warning: fopen returned 0x2 for file
     /sys/class/drm/card0-DSI-1/panel_info. (No such file or directory).
    
    Change-Id: Ic79af0c7a7db3bbfe0055c2769b00e63585970ff

--- include/drm/drm_bridge.h
+++ include/drm/drm_bridge.h
@@ -237,6 +237,7 @@ struct drm_bridge_funcs {
 	 * The enable callback is optional.
 	 */
 	void (*enable)(struct drm_bridge *bridge);
+
 };
 
 /**
@@ -313,6 +314,7 @@ void drm_bridge_mode_set(struct drm_bridge *bridge,
 			 struct drm_display_mode *mode,
 			 struct drm_display_mode *adjusted_mode);
 void drm_bridge_pre_enable(struct drm_bridge *bridge);
+
 void drm_bridge_enable(struct drm_bridge *bridge);
 
 #ifdef CONFIG_DRM_PANEL_BRIDGE
