2d26ffd32aa8751727a3456685becc1ae8e90ee5
predator112 <mi460334@gmail.com>
2021-09-20 13:00:58 +0000

    stop adding dirty

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#4 SMP PREEMPT Sun Sep 19 23:50:18 UTC 2021"
+#define UTS_VERSION "#5 SMP PREEMPT Mon Sep 20 05:51:13 UTC 2021"
 #define LINUX_COMPILE_BY "iqbal"
 #define LINUX_COMPILE_HOST "CircleCI"
 #define LINUX_COMPILER "Predator clang version 14.0.0 (https://github.com/llvm/llvm-project 4fc98ca6174ad173abc48646b713c91fd50e7182), GNU ld (GNU Binutils) 2.36.1"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.19.157[-Predator™]-gef6c38fa7f36"
+#define UTS_RELEASE "4.19.157[-Predator™]-g21387c46fbd0"
