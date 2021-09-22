dab1cb3086bc87aa559e88ba7773fe16d6183311
predator112 <mi460334@gmail.com>
2021-09-22 15:02:41 +0000

    stop adding dirty

--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.19.157[-Predator™]-ge0fb20273cda"
--- out/net/wireless/extra-certs.c
+++
@@ -1,5 +0,0 @@
-#include "reg.h"
-const u8 extra_regdb_certs[] = {
-
-};
-unsigned int extra_regdb_certs_len = sizeof(extra_regdb_certs);
