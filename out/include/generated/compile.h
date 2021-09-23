06f591b8069baa73c98781fb8df8319fabbe788a
predator112 <mi460334@gmail.com>
2021-09-23 09:43:54 +0000

    Revert "stop adding dirty"
    This reverts commit dab1cb3086bc87aa559e88ba7773fe16d6183311.

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#3 SMP PREEMPT Wed Sep 22 00:33:02 UTC 2021"
+#define LINUX_COMPILE_BY "iqbal"
+#define LINUX_COMPILE_HOST "CircleCI"
+#define LINUX_COMPILER "Predator clang version 14.0.0 (https://github.com/llvm/llvm-project 4fc98ca6174ad173abc48646b713c91fd50e7182), GNU ld (GNU Binutils) 2.36.1"
