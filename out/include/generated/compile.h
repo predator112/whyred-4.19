320410acffc416873769831648d67230c9bae107
predator112 <mi460334@gmail.com>
2021-09-25 00:22:50 +0000

    stop adding dirty

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 25 00:02:23 UTC 2021"
-#define LINUX_COMPILE_BY "iqbal"
-#define LINUX_COMPILE_HOST "CircleCI"
-#define LINUX_COMPILER "aarch64-linux-gnu-ld, "
